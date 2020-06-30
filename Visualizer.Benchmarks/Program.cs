f6b638bcc5b57df693b456fd459fda12fee94519
Alexander Boutin Flegel <alexander.boutinFlegel@propellerhead.co.nz>
2020-07-01 08:18:56 +1200

    added the benchmark runner

---
+++ Visualizer.Benchmarks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using DataFramework.Context.Seed;
+using DataFramework.Models;
+
+namespace Visualizer.Benchmarks
+{
+	public class Program
+    {
+		public static void Main(string[] args) => BenchmarkRunner.Run<SeedingBenchmark>();
+	}
+
+	public class SeedingBenchmark
+	{
+		public static (IEnumerable<int>, IEnumerable<int>, IEnumerable<int>) TwoOneZero => (new List<int> { 4, 5, 6 }, new List<int> { 4 }, new List<int> { 0 });
+
+		public (IEnumerable<Pool>, IEnumerable<Pool>) Pools { get; }
+
+		public SeedingBenchmark()
+		{
+			Pools = DiceSeed.SeedDice.SeedPools(TwoOneZero, TwoOneZero);
+		}
+
+		[Benchmark]
+		public List<PoolCombination> GenerateStats() => Pools.SeedCombinationStatistics().ToList();
+	}
+}
